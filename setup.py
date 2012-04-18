295e243af502e2f341f4f3e71a7cd6f1bcf781f0
Jonas Geiregat <jonas@geiregat.orgmkdir>
2012-04-18 20:14:58 +0200

    Addede description

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
 
-setup(name='django-404',
+setup(name='django-excepted',
         version='0.1',
-        description='',
+        description=("Middleware that catches and exception and excecutes it's render method"),
         author='Jonas Geiregat',
         author_email='jonas@geiregat.org',
         packages=['django_excepted']
